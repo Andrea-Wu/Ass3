837af5ff82009171f4522cdcfbf3a223ce7bb54b
fl273 <fan.liu3778@gmail.com>
2018-04-24 15:29:56 -0400

    library files

---
+++ libnetfiles.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include "libnetfiles.h"
+#include <errno.h>
+
+
+int netserverinit(char* hostname){
+  
+}
+
+int netopen(const char* pathname, int flags){
+
+}
+
+ssize_t netread(int fildes, void* buf, size_t nbyte){
+
+}
+
+
+ssize_t netwrite(int fildes, const void* buf, size_t nbyte){
+
+}
+
+
+int netclose(int fd){
+
+
+}
---
+++ libnetfiles.h
@@ -0,0 +1,10 @@
+#ifndef LIBNETFILES_H_
+#define LIBNETFILES_H_
+  
+  int netserverinit(char* hostname);
+  int netopen(const char* pathname, int flags);
+  ssize_t netread(int fildes, void* buf, size_t nbyte);
+  ssize_t netwrite(int fildes, const void* buf, size_t nbyte);
+  int netclose(int fd);
+  
+#endif
