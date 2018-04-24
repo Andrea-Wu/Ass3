f4fb76666066cdaa4bbbbebd8d60feec49683f3e
Andrea Wu <andrea.yh.wu@gmail.com>
2018-04-24 19:22:41 -0400

    server now connects to client

--- client.c
+++ client.c
@@ -13,26 +13,29 @@ int client(char* port){
     struct addrinfo *myAddr, *hints, *r;
     myAddr = (struct addrinfo*)malloc(sizeof(struct sockaddr));
     hints = (struct addrinfo*)malloc(sizeof(struct sockaddr));
-    hints -> ai_flags = AI_PASSIVE | AI_ADDRCONFIG;
+    hints -> ai_flags = AI_PASSIVE;
     hints -> ai_socktype = SOCK_STREAM;
     hints -> ai_family = PF_INET;
 
     int indic = getaddrinfo("less.cs.rutgers.edu", port, hints, &myAddr);
+    
     int cliSocket;
     for(r = myAddr; r; r -> ai_next){
-        cliSocket = socket(r -> ai_family, r -> ai_socktype, r -> ai_protocol);
+        cliSocket = socket(myAddr -> ai_family, myAddr -> ai_socktype, myAddr -> ai_protocol);
         
         //socket successfully created
-        if(cliSocket != -1){
+       if(cliSocket != -1){
             printf("great success %d\n", cliSocket);
 
             break;
         }
     }
 
-    int con = connect(cliSocket, r -> ai_addr, sizeof(r -> ai_addr)); 
-    printf("my connect: %d", con);
-    printf(
+    
+    int con = connect(cliSocket, r -> ai_addr, sizeof(*(r -> ai_addr))); 
+    printf("my connect: %d\n", con);
+    printf("%d\n", sizeof(*(r -> ai_addr)));
+    perror("wtf??:");
     //need sockaddr* for connect function. 
     //get addrinfo struct from func. how to convert?
     //sockaddr is a struct inside addrinfo: -> ai_addr (duh?)
